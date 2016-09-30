6f8a6528a58f22da1866b3b4c70c55de32988874
d.borisov <d.borisov@itexpert.ru>
2016-09-30 14:46:04 +0200

    Add  common projector

---
+++ HloMoney.Core/Projector/CommonProjector.cs
@@ -0,0 +1,33 @@
+﻿namespace HloMoney.Core.Projector
+{
+    #region Using Directives
+
+    using System;
+    using System.Linq.Expressions;
+    using Entity.Base;
+
+    #endregion
+
+    public class CommonProjector<TSource, TResult> : BaseProjector<TSource, TResult> where TSource : BaseEntity<TSource>
+    {
+        #region Private Fields
+
+        private readonly Expression<Func<TSource, TResult>> _expression;
+
+        #endregion
+
+        #region Constructor
+
+        public CommonProjector(Expression<Func<TSource, TResult>> expression)
+        {
+            this._expression = expression;
+        }
+
+        #endregion
+
+        public override Expression<Func<TSource, TResult>> ProjectExpr()
+        {
+            return this._expression;
+        }
+    }
+}
